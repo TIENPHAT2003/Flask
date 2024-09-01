376b9e364bce97024181136c6de4794327fabce6
TIENPHAT2003 <vutienphat1404@gmail.com>
2024-09-01 11:19:55 +0700

    Commit

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     mqtt_thread = Thread(target=run_mqtt)
     mqtt_thread.start()
     socketio.run(app,host="0.0.0.0", port=5000, use_reloader=True, debug=True)
-    # app.run(host='0.0.0.0', port=5050, debug=True)
+    
